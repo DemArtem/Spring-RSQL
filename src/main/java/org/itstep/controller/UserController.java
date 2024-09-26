d135c35b65068c825b1a426124eec91fc1dfcc65
gk <demar.demidov@yandex.ru>
2024-09-26 21:09:10 +0300

    test

--- src/main/java/org/itstep/controller/UserController.java
+++ src/main/java/org/itstep/controller/UserController.java
@@ -5,10 +5,14 @@ import cz.jirutka.rsql.parser.ast.Node;
 import org.itstep.repository.UserRepository;
 import org.itstep.rsql.CustomRsqlVisitor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.web.bind.annotation.*;
 import org.itstep.entity.*;
-
+import java.io.IOException;
 import java.util.List;
 
 @RestController
@@ -31,4 +35,56 @@ public class UserController {
         return repository.findAll(spec);
     }
     //http://localhost:8080/user/filter?search=id==2
+
+    @GetMapping(value = "/users", params = {"page", "size"})
+    public Page<User> paginationUsers(@RequestParam("page") int page,
+                                      @RequestParam("size") int size) throws IOException {
+        Pageable pageable = PageRequest.of(page, size);
+        return repository.findAll(pageable);
+    }
+    //http://localhost:8080/user/users?page=10&size=10
+
+    @GetMapping(value = "/users/filter", params = {"page", "size", "search"})
+    public Page<User> paginationFilterUsers(@RequestParam("page") int page,
+                                            @RequestParam("size") int size,
+                                            @RequestParam(value = "search", required = false) String search) {
+        Pageable pageable = PageRequest.of(page, size);
+        if (search != null) {
+            Node rootNode = new RSQLParser().parse(search);
+            Specification<User> spec = rootNode.accept(new CustomRsqlVisitor<User>());
+            return repository.findAll(spec, pageable);
+        } else return repository.findAll(pageable);
+    }
+    //http://localhost:8080/user/users/filter?page=1&size=10&search=id>100
+
+    @GetMapping(value = "/users/filter", params = {"page", "size", "search", "sort"})
+    public Page<User> paginationFilterUsers(
+            @RequestParam("page") int page,
+            @RequestParam("size") int size,
+            @RequestParam(value = "search", required = false) String search,
+            @RequestParam(value = "sort", required = false) String sort) {
+
+        Sort sortOrder = Sort.unsorted();
+        if (sort != null) {
+            String[] sortParams = sort.split(",");
+            for (String param : sortParams) {
+                String[] sortInfo = param.split(":");
+                        String property = sortInfo[0];
+                Sort.Direction direction = sortInfo.length > 1 && sortInfo[1].equalsIgnoreCase("desc") ? Sort.Direction.DESC : Sort.Direction.ASC;
+                sortOrder = sortOrder.and(Sort.by(direction, property));
+            }
+        }
+
+        Pageable pageable = PageRequest.of(page, size, sortOrder);
+
+        if (search != null) {
+            Node rootNode = new RSQLParser().parse(search);
+            Specification<User> spec = rootNode.accept(new CustomRsqlVisitor<User>());
+            return repository.findAll(spec, pageable);
+        } else {
+            return repository.findAll(pageable);
+        }
+    }
+    //http://localhost:8080/user/users/filter?page=1&size=10&search=id>100&sort=surname
+
 }
